107acf721211cf760e22e21454332d9225a6dfdb
Nick Teets <nickteets@gmail.com>
2018-02-07 21:16:32 -0700

    Add links to contact

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ const flyOverMediaHandler = () => {
   window.open('http://flyovermedia.org', '_blank');
 };
 
+const githubHandler = () => {
+  window.open('https://github.com/nicktu12', '_blank');
+};
+
+const linkedinHandler = () => {
+  window.open('https://linkedin.com/in/nickteets', '_blank');
+};
+
 $('.statify').on('click', statifyHandler);
 $('.construction-forms').on('click', constructionFormsHandler);
 $('.fly-over-media').on('click', flyOverMediaHandler);
+$('.github-icon').on('click', githubHandler);
+$('.linkedin-icon').on('click', linkedinHandler);
