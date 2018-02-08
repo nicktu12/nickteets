5d29b3bd9d6a611a0af70b3fdbe4df927fd04d99
Nick Teets <nickteets@gmail.com>
2018-02-07 17:12:51 -0700

    Add handlers for FOM and con forms

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ const statifyHandler = () => {
   window.open('http://statify12.herokuapp.com/', '_blank');
 };
 
+const constructionFormsHandler = () => {
+  window.open('http://construction-forms.herokuapp.com/', '_blank');
+};
+
+const flyOverMediaHandler = () => {
+  window.open('http://flyovermedia.org', '_blank');
+};
+
 $('.statify').on('click', statifyHandler);
+$('.construction-forms').on('click', constructionFormsHandler);
+$('.fly-over-media').on('click', flyOverMediaHandler);
